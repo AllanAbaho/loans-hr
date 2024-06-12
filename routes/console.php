d79526bdc5f8f801e82959b295a149cfcebd6b69
Allan Abaho <abahoallans@gmail.com>
2024-06-12 12:14:26 +0300

    removed every 3 minutes method

--- routes/console.php
+++ routes/console.php
@@ -20,4 +20,4 @@ Artisan::command('add:user', function () {
         'role' => 'Employee'
     ]);
     Log::info('Added new user ', ['name' => $name]);
-})->purpose('Add new user to organisation')->everyThreeMinutes();
+})->purpose('Add new user to organisation');
